e6c22326358aeb49b085b1f56b25c0ea89d30fb1
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-07-05 19:52:53 -0500

    chore: update MongoDB connection to use adoption_db with authSource

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const mongoURI = 'mongodb://admin:Lis12345@54.156.27.209:27017/admin';
+  const mongoURI = 'mongodb://admin:Lis12345@54.156.27.209:27017/adoption_db?authSource=admin';
 
   try {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log('MongoDB connected to adoption_db');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1);
